3648027afef99f12d7c8e91973d77116fb5616fc
Izuagbe Samuel <izuagbesamuel73@gmail.com>
2021-05-07 16:56:53 +0100

    Add html document

--- js/script-BLI.js
+++ js/script-BLI.js
@@ -1,18 +1,26 @@
-function pigLatin(sound) {
-  sound = sound.toLowerCase();
-  let vowels = ["a", "e", "i", "o", "u"];
+function pigLatin(str) {
+  str = str.toLowerCase()
+  const vowels = ["a", "e", "i", "o", "u"];
   let vowelIndex = 0;
 
-  if (vowels.includes(sound[0])) {
+  if (vowels.includes(str[0])) {
     return str + "way";
   } else {
-    for (let char of sound) {
+    for (let char of str) {
       if (vowels.includes(char)) {
-        vowelIndex = sound.indexOf(char);
+        vowelIndex = str.indexOf(char);
         break;
       }
     }
-
-    return sound.slice(vowelIndex) + sound.slice(0, vowelIndex) + "ay";
+    return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
   }
+}
+
+function pigQu(word) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  for(var i = 0; i < word.length; i++) {
+    if(vowels.indexOf(word[i]) !== -1) break;
+    if(word.slice(i, i+2) === 'qu') i += 1;
+  };
+  return word.slice(i) + word.slice(0, i) + 'ay';
 }
