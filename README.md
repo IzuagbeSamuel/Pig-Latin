736529dd53e603d75efcd5e84da029e3b76ca2d9
Izuagbe Samuel <izuagbesamuel73@gmail.com>
2021-05-07 15:01:42 +0100

    Add BIL to script, It will add ay to the end of a consonant

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ Describe: pigLatin()
 Test: "It wil add 'way' to the end of words that begin with a vowel."
 Code: pigLatin("a");
 Expected Output: "away"
+
+
+Test: "It wil add 'ay' to the end of words that begin with a consonant."
+Code: pigLatin("school");
+Expected Output: "ooschlay"
+
+
+Test: "It wil add 'ay' to the end of words that begin with a word that has qu."
+Code: pigLatin("quite");
+Expected Output: "itequay"
--- js/script-BLI.js
+++ js/script-BLI.js
@@ -1,8 +1,18 @@
 function pigLatin(sound) {
   sound = sound.toLowerCase();
+  let vowels = ["a", "e", "i", "o", "u"];
+  let vowelIndex = 0;
 
-  if (["a", "e", "i", "o", "u"].indexOf(sound[0]) > -1) {
-    return (sound = sound + "way");
+  if (vowels.includes(sound[0])) {
+    return str + "way";
+  } else {
+    for (let char of sound) {
+      if (vowels.includes(char)) {
+        vowelIndex = sound.indexOf(char);
+        break;
+      }
+    }
+
+    return sound.slice(vowelIndex) + sound.slice(0, vowelIndex) + "ay";
   }
-  return sound;
 }
